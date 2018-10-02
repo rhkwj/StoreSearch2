3e11f7ec221fe5a62fc846f70b057c9aa4620c8d
rhkwj <rhkwj@naver.com>
2018-10-02 16:41:11 +0800

    Initial Commit

---
+++ StoreSearch2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StoreSearch2
+//
+//  Created by Kim Yeon Jeong on 2018/10/2.
+//  Copyright © 2018 Kim Yeon Jeong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
