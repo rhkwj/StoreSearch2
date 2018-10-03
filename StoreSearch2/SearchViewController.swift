6c09e392da7721ba67a18fc5932450edfa9bea67
rhkwj <rhkwj@naver.com>
2018-10-03 09:59:09 +0800

    page 788

--- StoreSearch2/SearchViewController.swift
+++ StoreSearch2/SearchViewController.swift
@@ -22,3 +22,13 @@ class SearchViewController: UIViewController {
 
 }
 
+extension SearchViewController: UISearchBarDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView,
+                   numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    func tableView(_ tableView: UITableView,
+                   cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+}
