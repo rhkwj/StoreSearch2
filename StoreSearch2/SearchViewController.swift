fc278f0ae361413c272e374b12ceb82be93a680f
rhkwj <rhkwj@naver.com>
2018-10-03 15:49:04 +0800

    page 805

--- StoreSearch2/SearchViewController.swift
+++ StoreSearch2/SearchViewController.swift
@@ -35,6 +35,9 @@ class SearchViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         tableView.contentInset = UIEdgeInsets(top: 64, left: 0,bottom: 0, right: 0)
+        let cellNib = UINib(nibName: "SearchResultCell", bundle: nil)
+        tableView.register(cellNib, forCellReuseIdentifier:
+            "SearchResultCell")
     }
 
 
