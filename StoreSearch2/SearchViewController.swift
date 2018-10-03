d14161782a1eca1a611f6aa5d37428e3f894a28a
rhkwj <rhkwj@naver.com>
2018-10-03 09:38:26 +0800

    page 785

--- StoreSearch2/SearchViewController.swift
+++ StoreSearch2/SearchViewController.swift
@@ -8,11 +8,15 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class SearchViewController: UIViewController {
 
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        tableView.contentInset = UIEdgeInsets(top: 64, left: 0,bottom: 0, right: 0)
     }
 
 
