d7e92599e93af55215501c1c2f79546c03b79111
rhkwj <rhkwj@naver.com>
2018-10-04 15:53:14 +0800

    page 817

--- StoreSearch2/SearchResultCell.swift
+++ StoreSearch2/SearchResultCell.swift
@@ -14,4 +14,10 @@ class SearchResultCell: UITableViewCell {
     @IBOutlet weak var artistNameLabel: UILabel!
     @IBOutlet weak var artworkImageView: UIImageView!
 
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        let selectedView = UIView(frame: CGRect.zero)
+        selectedView.backgroundColor = UIColor(red: 20/255, green: 160/255, blue: 160/255, alpha: 0.5)
+        selectedBackgroundView = selectedView
+    }
 }
