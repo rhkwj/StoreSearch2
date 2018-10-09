7666fd5d4b307a2f5a73fa8a4a5eda68cc40f67e
rhkwj <rhkwj@naver.com>
2018-10-09 09:22:42 +0800

    page 841

--- StoreSearch2/SearchResult.swift
+++ StoreSearch2/SearchResult.swift
@@ -8,11 +8,14 @@
 import UIKit
 import Foundation
 
-
-
-class SearchResult {
-    
-    var name = ""
+class ResultArray:Codable {
+    var resultCount = 0
+    var results = [SearchResult]()
+}
+class SearchResult:Codable {
     var artistName = ""
-    
+    var trackName = ""
+    var name:String {
+        return trackName
+    }
 }
