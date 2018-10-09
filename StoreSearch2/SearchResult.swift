3ac1bab44caf44562c6e62ec6f1dc0e9fec3d9c1
rhkwj <rhkwj@naver.com>
2018-10-09 10:10:24 +0800

    page 845

--- StoreSearch2/SearchResult.swift
+++ StoreSearch2/SearchResult.swift
@@ -12,10 +12,14 @@ class ResultArray:Codable {
     var resultCount = 0
     var results = [SearchResult]()
 }
-class SearchResult:Codable {
+class SearchResult:Codable, CustomStringConvertible {
     var artistName = ""
     var trackName = ""
     var name:String {
         return trackName
+    }
+    
+    var description:String {
+        return "Name: \(name), Artist Name: \(artistName)"
     }
 }
