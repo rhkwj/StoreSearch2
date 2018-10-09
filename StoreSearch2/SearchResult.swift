66887515c3d5b10aeebd24d301a97ea11f997b08
rhkwj <rhkwj@naver.com>
2018-10-09 11:37:56 +0800

    page 853 stuck

--- StoreSearch2/SearchResult.swift
+++ StoreSearch2/SearchResult.swift
@@ -15,28 +15,48 @@ class ResultArray:Codable {
 class SearchResult:Codable, CustomStringConvertible {
     var kind = String?
     var artistName = ""
-    var trackName = ""
-    var trackPrice = 0.0
+    var trackName:String?
+    var trackPrice:Double?
     var currency = ""
-    
+    var trackViewUrl:String?
+    var collectionName:String?
+    var collectionViewUrl:String?
+    var collectionPrice:Double?
+    var itemPrice:Double?
+    var itemGenre:String?
+    var bookGenre:[String]?
     var imageSmall = ""
     var imageLarge = ""
-    var storeURL = ""
-    var genre = ""
+    
     enum CodingKeys: String, CodingKey {
         case imageSmall = "artworkUrl60"
         case imageLarge = "artworkUrl100"
         case storeURL = "trackViewUrl"
         case genre = "primaryGenreName"
+        case itemPrice = "price"
         case kind, artistName, trackName
         case trackPrice, currency
     }
     
     var name:String {
-        return trackName
+        return trackName ?? collectionName ?? ""
     }
     
     var description:String {
         return "Kind: \(kind ?? ""), Name: \(name), Artist Name: \(artistName)\n"
     }
+    
+    var storeURL:String {
+        return trackViewUrl ?? collectionViewUrl ?? ""
+    }
+    var price:Double {
+        return trackPrice ?? collectionPrice ?? itemPrice ?? 0.0
+    }
+    var genre:String {
+        if let genre = itemGenre {
+            return genre
+        } else if let genres = bookGenre {
+            return genres.joined(separator: ", ")
+        }
+        return "" }
 }
