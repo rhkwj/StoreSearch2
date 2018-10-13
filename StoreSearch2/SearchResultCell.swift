1a40e682aa22737da9c5cc00b3b7cb33b1e6707b
rhkwj <rhkwj@naver.com>
2018-10-13 11:15:09 +0800

    page 900

--- StoreSearch2/SearchResultCell.swift
+++ StoreSearch2/SearchResultCell.swift
@@ -20,4 +20,16 @@ class SearchResultCell: UITableViewCell {
         selectedView.backgroundColor = UIColor(red: 20/255, green: 160/255, blue: 160/255, alpha: 0.5)
         selectedBackgroundView = selectedView
     }
+    
+    // MARK:- Public Methods
+    func configure(for result: SearchResult) {
+        nameLabel.text = result.name
+        if result.artistName.isEmpty {
+            artistNameLabel.text = "Unknown"
+        } else {
+            artistNameLabel.text = String(format: "%@ (%@)", result.artistName, result.type)
+        }
+        
+    }
+    
 }
