7ad24ef2431e56d5b859407a3670ae903b436c9d
rhkwj <rhkwj@naver.com>
2018-10-15 11:00:13 +0800

    page 919 and not yet do link part

---
+++ StoreSearch2/DimmingPresentationController.swift
@@ -0,0 +1,9 @@
+//
+//  DimmingPresentationController.swift
+//  StoreSearch2
+//
+//  Created by Kim Yeon Jeong on 2018/10/15.
+//  Copyright © 2018 Kim Yeon Jeong. All rights reserved.
+//
+
+import Foundation
