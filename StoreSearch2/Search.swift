698b7b709380272c4f7f05b2e28cbe88bfe528be
rhkwj <rhkwj@naver.com>
2018-10-30 09:03:56 +0800

    page 1035

--- StoreSearch2/Search.swift
+++ StoreSearch2/Search.swift
@@ -85,12 +85,18 @@ class Search {
     // MARK:- Private Methods
     private func iTunesURL(searchText: String,
                            category: Category) -> URL {
+        // Add the following 3 lines
+        let locale = Locale.autoupdatingCurrent
+        let language = locale.identifier
+        let countryCode = locale.regionCode ?? "en_US"
         let kind = category.type
         let encodedText = searchText.addingPercentEncoding(
             withAllowedCharacters: CharacterSet.urlQueryAllowed)!
         let urlString = "https://itunes.apple.com/search?" +
-        "term=\(encodedText)&limit=200&entity=\(kind)"
+            "term=\(encodedText)&limit=200&entity=\(kind)" +
+        "&lang=\(language)&country=\(countryCode)"
         let url = URL(string: urlString)
+        print ("URL: \(url!)") //Add this
         return url!
     }
     
