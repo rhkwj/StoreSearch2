f5ae431d5008e07859f16fda9b70e45c2b55dfa3
rhkwj <rhkwj@naver.com>
2018-10-31 13:59:35 +0800

    page 1050

--- StoreSearch2/LandscapeViewController.swift
+++ StoreSearch2/LandscapeViewController.swift
@@ -224,6 +224,7 @@ class LandscapeViewController: UIViewController {
                     as! DetailViewController
                 let searchResult = list[(sender as! UIButton).tag - 2000]
                 detailViewController.searchResult = searchResult
+                detailViewController.isPopUp = true
             }
         }
     }
