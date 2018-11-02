58b0ac4637ecb9f4ed578d0d4373199c0e8b8b88
rhkwj <rhkwj@naver.com>
2018-11-02 13:17:17 +0800

    page 1070

--- StoreSearch2/DetailViewController.swift
+++ StoreSearch2/DetailViewController.swift
@@ -182,6 +182,8 @@ extension DetailViewController: MenuViewControllerDelegate {
         dismiss(animated: true) {
             if MFMailComposeViewController.canSendMail() {
                 let controller = MFMailComposeViewController()
+                controller.mailComposeDelegate = self
+                controller.modalPresentationStyle = .formSheet
                 controller.setSubject(NSLocalizedString("Support Request", comment: "Email subject"))
                 controller.setToRecipients(["your@email-address-here.com"])
                 self.present(controller, animated: true, completion: nil)
@@ -189,4 +191,11 @@ extension DetailViewController: MenuViewControllerDelegate {
         }
     }
 }
+
+extension DetailViewController: MFMailComposeViewControllerDelegate {
+    func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+        dismiss(animated: true, completion: nil)
+    }
+}
+
 
